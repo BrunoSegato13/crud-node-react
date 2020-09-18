78069c73337f14bf01c96d9f55d28cb82481b284
BrunoSegato13 <brunocgato@hotmail.com>
2020-09-17 23:34:22 -0300

    Adição do método select

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const UserController = require('../controllers/userController');
 
 router.post('/usuario', UserController.Insert);
+router.get('/usuario', UserController.SearchAll);
+router.get('/usuario/:id', UserController.SearchOne);
 
 module.exports = router;
