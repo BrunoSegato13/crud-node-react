cc54edb38a40337c06440dac106c833c20053442
BrunoSegato13 <brunocgato@hotmail.com>
2020-09-19 17:18:59 -0300

    Metódos update e delete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,8 @@ const UserController = require('../controllers/userController');
 router.post('/usuario', UserController.Insert);
 router.get('/usuario', UserController.SearchAll);
 router.get('/usuario/:id', UserController.SearchOne);
+router.put('/usuario/:id', UserController.Update);
+router.delete('/usuario/:id', UserController.Delete);
+
 
 module.exports = router;
