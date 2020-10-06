67859d18db40c465741aaf057cb957180bd86528
BrunoSegato13 <brunocgato@hotmail.com>
2020-10-05 23:21:02 -0300

    Fix back-end

---
+++ back-End/src/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/userController');
+
+router.post('/usuario', UserController.Insert);
+router.get('/usuario', UserController.SearchAll);
+router.get('/usuario/:id', UserController.SearchOne);
+router.put('/usuario/:id', UserController.Update);
+router.delete('/usuario/:id', UserController.Delete);
+
+
+module.exports = router; 
